2381537bddf537d4200ea9f4c0cf1c49c6d150c3
Divya1234jain <99873820+Divya1234jain@users.noreply.github.com>
2022-04-01 10:42:41 +0530

    added files

---
+++ index.js
@@ -0,0 +1,21 @@
+// import square from './square';
+
+// //const square = require('./square');
+// // console.log(square);
+
+// let squareData = square.square(2);
+// console.log(squareData);
+
+
+// let addRes = square.add(2, 3);
+//  console.log(addRes)
+
+// const express = require('express');
+ //import animal from botan
+ const animal = require('botanic-zoo-api')
+animal.getAnimal('lion')
+  .then(response => console.log(response))
+  .catch(err => console.error(err))
+
+
+//import { chat } from "chat";
---
+++ square.js
@@ -0,0 +1,13 @@
+
+function square(number) {
+    return number * number;
+}
+
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+module.exports = {
+    square : square,
+    add : add
+}
